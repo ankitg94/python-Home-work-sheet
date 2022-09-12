63ffd17bf821382e949eff892e31eeadca88f9ec
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-12 08:59:23 +0530

    Add files via upload
    assigment 1 solution of saurabh sukla sir web development course

---
+++ assigment1.py
@@ -0,0 +1,28 @@
+#answer-4
+print("Hello Python")
+#answer-5
+print("hello")
+print("Python")
+#answer -6
+print('"mySirG"')
+#answer-7
+print('"Teacher''s day"')
+#answer-8
+a="ankit"
+print("my name is :",a)
+#answer-9
+name="ankit"
+age = 22
+qual="B.Tech"
+exper="fresher"
+print("my name is :",name)
+print("my age is :",age)
+print("my Qualification is :",qual)
+print("my experience is :",exper)
+#answer -10
+a=9
+b=7
+c=5
+print(a)
+print(b)
+print(c)
