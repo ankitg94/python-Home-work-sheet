9a16b813e02994d6a50eef3f1e6b8d09439b7537
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-12 09:00:23 +0530

    Add files via upload

---
+++ assigment2.py
@@ -0,0 +1,48 @@
+#answer-1
+"""
+print('"learning python"')
+"""
+#answer -2
+"""
+this 
+is the
+ multi 
+ line comment
+"""
+"""
+a=2;b=3.5;c="ankit";d=True
+print(a);print(b);print(c);print(d)
+"""
+#answer -3
+"""
+a=35;b=True;c="MySirG";d=5.46;e= 3+4j
+print(a);print(b);print(c);print(d);print(e)
+"""
+#answer-4
+"""
+x=8
+y=8
+print(id(x))
+print(id(y))
+"""
+#answer-5
+"""
+q=12;u=3.7;x="ankit Gupta";z=True
+print(type(q))
+print(id(q))
+print(type(u))
+print(id(u))
+print(type(x))
+print(id(x))
+print(type(z))
+print(id(z))
+"""
+#answer-6
+"""
+import keyword
+print(keyword.kwlist)
+"""
+#answer-10
+from datetime import datetime
+now = datetime.now()
+print("now =",now)
