ac6473746c4240eaebf6480694c9f230955d07a1
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-13 14:41:18 +0530

    Add files via upload

---
+++ assigment4.py
@@ -0,0 +1,50 @@
+
+#answer-1
+a=input("Enter your name :")
+print(a)
+#answer-2
+b=int(input("Enter the number :"))
+print(b)
+#answer-3
+c=int(input("Enter the number :"))
+d=int(input("Enter the number :"))
+print("sum of two number :",c+d)
+
+#answer-4
+e=int(input("Enter the radius : "))
+print("Area of circle :",3.14*e*e)
+
+#answer-5
+f=int(input("Enter the number :"))
+print("square of ",f,"is :",f*f)
+
+#answer-6
+#by using b&h
+g=int(input("Enter base of triangle :"))
+h=int(input("Enter height of triangle: "))
+print("Area of triangle:",0.5*g*h)
+
+#answer -7
+i=int(input("Enter the 1st no :"))
+j=int(input("Enter the 2nd no :"))
+k=int(input("Enter the 3rd no :"))
+print("Average of Three number :",(i+j+k)/3)
+
+#answer-8
+l=int(input("Enter the principal amount:"))
+m=int(input("Enter the rate of interest:"))
+n=int(input("Enter the time period :"))
+k=(l*m*n)/100
+print("Simple interst:",k)
+print("Total Amount after",n,"year:",k+l)
+
+#answer-9
+o=int(input("Enter the length :"))
+p=int(input("Enter the breadth :"))
+q=int(input("Enter the height :"))
+print("Volume of cuboid :",o*p*q)
+
+#answer-10
+x=int(input("Enter the length :"))
+y=int(input("Enter the breadth :"))
+print("Area of rectangle :",x*y)
