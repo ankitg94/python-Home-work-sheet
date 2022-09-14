23d4efe8dd98313c221d72c57e54b6163b9af64f
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-14 17:33:30 +0530

    Add files via upload

---
+++ assigment5.py
@@ -0,0 +1,44 @@
+
+#answer-1
+a=2534
+print(a//10)
+#answer-2
+a="2089"
+print(a[-1])
+#answer-3
+b=5
+c=8
+print([b][c] and [c][b])
+
+#answer-4
+d=int(input("Enter the number :"))
+e=int(input("Enter the expo :"))
+print("result :",d**e)
+
+#answer-5
+f=int(input("Enter the 1st digit :"))
+g=int(input("Enter the 2nd digit :"))
+h= int(input("Enter the 3rd digit:"))
+print("output 1st digit ",f)
+#answer-6
+f=int(input("Enter the 1st digit :"))
+g=int(input("Enter the 2nd digit :"))
+h= int(input("Enter the 3rd digit:"))
+print("output 1st digit ",g)
+#answer-7
+f=int(input("Enter the 1st digit :"))
+g=int(input("Enter the 2nd digit :"))
+h= int(input("Enter the 3rd digit:"))
+print("output 1st digit ",h)
+
+#answer-8
+i=3,5,6,9,87
+print(3 in i)
+#answer-9
+j=5,9,7,2
+print(8 not in j)
+#answer-10
+a=9
+b=9
+print(a is b)
+print(a is not b )
