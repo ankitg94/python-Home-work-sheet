6ccd16a005e2c4aa72c8170e3cfda069ba11ffd2
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-15 16:40:16 +0530

    Add files via upload

---
+++ assigment8.py
@@ -0,0 +1,59 @@
+#While loop
+#answer-1
+i=1
+while i<=5:
+     print("MySirG")
+     i+=1
+#answer-2
+i=1
+while i<=10:
+    print(i,end=' ')
+    i+=1
+    sep='\\n'
+#answer-3
+i=10
+while i>=1:
+    print(i,end=' ')
+    i-=1    
+#answer-4
+#10 odd natural no
+i=1
+while i<=20:
+    print(i) 
+    i+=2 
+#answer-5
+#reverse of 10 odd 
+i=19
+while i>=1:
+    print(i)
+    i-=2
+#answer-6
+#10 even no
+i=1
+while i<=10:
+    print(i*2)
+    i+=1     
+#answer-7
+#reverse even natural no
+i=10
+while i>=1:
+    print(i*2)
+    i-=1
+#answer-8
+#square of 10  natural no
+i=1
+while i<=10:
+    print(i*i)
+    i+=1    
+#answer-9
+#cube of 10 natural no
+i=1
+while i<=10:
+    print(i*i*i)
+    i+=1
+#answer-10
+#multiple of 5
+i=1
+while i<=10:
+    print(i*5)
+    i+=1
