901296f4f5024c77977c77430a7bc0e708f11012
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-15 16:40:46 +0530

    Add files via upload

---
+++ assigment9.py
@@ -0,0 +1,64 @@
+#answer-1
+
+a=int(input("Enter the no :"))
+i=1
+while i<=a:
+    print("MysirG")
+    i+=1
+    
+#answer-2
+b=int(input("Enter the no:"))
+i=1
+while i<=b:
+    print(i)
+    i+=1    
+
+#answer-3
+c= int(input("Enter the no : "))
+i=c
+while i>=1:
+    print(i) 
+    i-=1   
+
+#answer-4
+d=int(input("Enter the no :"))
+i=1
+while i<=d:
+    print(i)
+    i+=2 
+   
+#answer-5
+#pending
+e=int(input("Enter the no :"))
+i=e       
+while i>=1:
+    print(i)
+    i-=2
+
+#answer-6
+f= int(input("Enter the no :"))
+i=0
+while i<=f:
+    print(i)
+    i+=2    
+
+#answer-8
+g=int(input("Enter the no :"))
+i=1
+while i<=g:
+    print(i*i)
+    i+=1
+
+#answer-9
+h= int(input("Enter the no :"))
+i=1
+while i<=h:
+    print(i*i*i)
+    i+=1
+
+#answer-10
+j=int(input("Enter the no :"))
+i=1
+while i<=10:
+    print(j*i)
+    i+=1
