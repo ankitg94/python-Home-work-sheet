7f2373792b09f5b485bcbb1c53c64c3fedce8d75
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-16 13:50:36 +0530

    Add files via upload

---
+++ assigment10.py
@@ -0,0 +1,48 @@
+#answer-1
+a=int(input("Enter the no :"))
+for i in range(a):
+    print("MysirG")
+#answer-2
+q=int(input("Enter the no:))
+for i in range(1,q,1)    
+   
+#answer-3   
+b=int(input("Enter the no :"))
+for i in range(b,0,-1):
+     print(i)
+
+#answer-4
+c= int(input("Enter the no :"))
+for i in range(1,c+1,2):
+    print(i)
+
+#answer-5
+d=int(input("Enter the no :"))
+for i in range(d,0,-2):
+    print(i)
+
+#answer-6
+e=int(input("Enter the no :"))
+for i in range(2,e+1,2):
+    print(i)
+
+#answer-7
+#pending
+f=int(input("Enter the no :"))
+for i in range(f,1,-2):
+    print(i)
+
+#answer-8
+g=int(input("Enter the no : "))
+for i in range(1,g+1):
+    print(i*i)
+
+#answer-9
+h=int(input("Enter the no :"))
+for i in range(1,h+1):
+    print(i*i*i)      
+
+#answe-10
+j=int(input("Enter the no :"))
+for i in range(1,11):
+    print(i*j)    
