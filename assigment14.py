49c69aabdb49bc1089d4513c7bbab22eba83bb9a
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-17 11:13:02 +0530

    Add files via upload

---
+++ assigment14.py
@@ -0,0 +1,34 @@
+#answer-1
+"""
+a=int(input("Enter the no :"))
+for i in range(1,a+1):
+    print(i)
+#answer-2
+b=int(input("Enter the no:"))
+for i in range(1,b,2):
+    print(i)
+
+#answer-3
+c=int(input("Enter the input:"))
+for i in range(0,c+1,2):
+    print(i)    
+
+#answer-4&5
+d=[2,5,7,9,4,1]
+print(max(d))
+print(min(d)) 
+
+#answer-6
+e=int(input("Enter the 1st no :"))
+f=int(input("Enter the  2nd no :"))
+g=int(input("Enter the 3rd no :"))
+h=[e+f+g]
+print(h)  
+"""
+#answer-9
+l=[2,5,9,8,4,1,2,7]
+print(l.count(7))
+#answer-10
+l1=[2,50,16,9,7]
+l1.sort()
+print(l1)
