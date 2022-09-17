7c5f21c02489b7a9b23a2d3f572c6551eb680ee0
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-17 20:16:33 +0530

    Add files via upload

---
+++ assigment17.py
@@ -0,0 +1,40 @@
+#set-chapter
+#answer-1
+a={"python","c","c++","html"}
+print(a)
+#answer-2
+#pending
+b=input("Enter your personal detail :")
+b1={eval(e) for e in b}
+print(b1)
+
+#answer-3
+c={2,5,4,7}
+print(type(c))
+
+#answer-4
+d={"python","java","django"}
+d1={e for e in d if e in"python"}
+print(d1)
+
+#answer-5
+e={"java","python","Sql"}
+e.update(["c"],["cpp"],["no sql"])
+print(e)
+
+#naswer-6
+f={"python","django","javaScript"}
+f.add(2)
+print(f)
+
+#answer-8
+g={3,4,'abc'}
+g.clear()
+print(g)
+
+#answer-10
+h={2,6,3,5,9}
+h1=max(h)
+h2=min(h)
+print(h1)
+print(h2)
