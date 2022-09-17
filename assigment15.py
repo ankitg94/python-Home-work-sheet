b3e26293a2606e4622e0247fe98b8d44c3dc4b71
ankit gupta <103044611+ankitg94@users.noreply.github.com>
2022-09-17 15:22:27 +0530

    Add files via upload

---
+++ assigment15.py
@@ -0,0 +1,34 @@
+#answer-1
+print(type('ankit'))
+print(type("Ankit"))
+print(type('''ankit'''))
+#answer-2
+a=("iNeuron")
+print(a[0:5])
+#answer-3
+b=("Hello learners")
+print(b[2:5])
+#answer-4
+c=("learning ")
+d=("python")
+print(c+d)
+#answer-5
+e='ineuron'
+print(e.count(''))
+#answer-6
+print(e[::-1])
+#answer-7
+a="ankit ia an ashiq boy"
+b="ashiq"
+if b in a:
+    print("found")
+else:
+    print("lost")    
+#answer-8
+g=("a1s24dc")
+print(g.isdigit())    
+#answer-9
+print(g.isalpha())
+#answer-10
+a=1024
+print(type(str(a)))
